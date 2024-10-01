06faf7a80f8573b6e39fa50951c7853de306c86b
LucasSouzaG <lucassouza.g@hotmail.com>
2024-10-01 20:58:20 -0300

    test input.stream

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
-from compiler.lexer import Lexer
-from compiler.parser import Parser
-from compiler.codegen import CodeGen
-from compiler.run_code import  run_code
+# coding: utf8
+import sys
+import os
 
-fname = "input.stream"
+# Verificar se um argumento foi passado
+if len(sys.argv) < 2:
+    print("Uso: python main.py <nome_do_arquivo>")
+    sys.exit(1)
 
-with open(fname) as f:
-    text_input = f.read()
+# limpar tela
+os.system("cls")
 
-lexer = Lexer().get_lexer()
-tokens = lexer.lex(text_input)
+dicionario = [("Corra", "for"),
+              ("na", "in"),
+              (" faca", ":"),
+              ("AteoUltimoHomem", "while"),
+              ("TopaOuNaoTopa ", "elif "),
+              ("eAssimQueAcaba", "else:"),
+              ("entao", ":"),
+              ("batmanBegins ", "if "),
+              ("BobOConstrutor", "def"),
+              (" e ", " and "),
+              (" ou ", " or "),
+              ("NaoDurma(", "not("),
+              ("SemMedoDaVerdade", "True"),
+              ("FalsaIdentidade", "False"),
+              ("theOffice", "print"),
+              ("oRetornoDoJedi", "return"),
+              ("Valente", "range")]
 
-codegen = CodeGen()
+cabecalho = "#coding: utf8\nimport math\ndef ler(texto):\n  t = raw_input(texto)\n  if t.isdigit():\n    return " \
+            "float(t)\n  else:\n    return t\ndef lista(inicio, fim, passo=1):\n  return range(inicio, fim+1, " \
+            "passo)\ndef raiz(n, r):\n  return n ** (1/float(r))\ndef texto(n):\n  return str(n)\n"
 
-module = codegen.module
-builder = codegen.builder
-printf = codegen.printf
+with open(f"{sys.argv[1]}.py", 'w') as f:
+    f.write(cabecalho)
 
-pg = Parser(module, builder, printf)
-pg.parse()
-parser = pg.get_parser()
-parser.parse(tokens).eval()
+with open(sys.argv[1], 'r') as fonte:
+    for linha in fonte:
+        for (velho, novo) in dicionario:
+            linha = linha.replace(velho, novo)
+        with open(f"{sys.argv[1]}.py", 'a') as f:  # Abrir em modo de anexação
+            f.write(linha)
 
-codegen.create_ir()
-codegen.save_ir("output.ll")
-
-with open('compiler\output\output.ll') as f:
-    run_code(f.read())
+# Executar o arquivo gerado
+os.system(f"python {sys.argv[1]}.py")
